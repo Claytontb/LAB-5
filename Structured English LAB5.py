2f28f287a606729e8bc7342f0521ca9127a33d69
Claytontb <78582298+Claytontb@users.noreply.github.com>
2021-05-09 15:24:20 -0700

    Add files via upload

---
+++ Structured English LAB5.py
@@ -0,0 +1,22 @@
+Structured English LAB5
+
+"""
+main()
+    set sentence = input()
+    set dictionary = create_dictionary()
+    translate(sentence, dictionary)
+
+translate(sentence, dictionary):
+    words = for each of the word in the sentence 
+    for the words, translate the word 
+    print translated sentence to user
+
+create_dictionary()
+    read in textese.txt 
+    create list = each line from the file 
+    close the file 
+    create a dict off of the list 
+    return to dict 
+
+    main()
+"""
